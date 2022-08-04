872abe78da00fe8fbef7e164376f84bf5fa8c2c8
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-08-04 07:32:50 +0100

    first commit

---
+++ src/mockProducst.js
@@ -0,0 +1,70 @@
+const shoeList = [
+  {
+    id: 1,
+    title: 'Foot Cushion',
+    price: {
+      currencyUnit: {
+        code: 'GBP',
+        numericCode: 826,
+        decimalPlaces: 2,
+        numeric3Code: '826',
+        countryCodes: ['GG', 'IM', 'GB', 'JE'],
+        pseudoCurrency: false,
+        symbol: '£',
+      },
+      amount: 13.87,
+      amountMajor: 13,
+      amountMajorLong: 13,
+      amountMajorInt: 13,
+      amountMinor: 1387,
+      amountMinorLong: 1387,
+      amountMinorInt: 1387,
+      minorPart: 87,
+      positiveOrZero: true,
+      negativeOrZero: false,
+      positive: true,
+      scale: 2,
+      zero: false,
+      negative: false,
+    },
+    productCategory: 'Mens',
+    images: ['url1', 'url2', 'url3'],
+    shortDescription: 'High quality foot cushion for all your cushiony needs',
+    longDescription:
+      'These Timberland shoes are classic and contemporary at the same time. The Stormbuck style will not only make you look and feel good, but is also environmentally friendly. As well as using recycled rubber for the soles, the linings have been made from recycled plastic bottles, creating an eco-friendly product.',
+    rating: {
+      averageRate: 4.5,
+      count: 1500,
+    },
+    specifications: [
+      {
+        mainShoeMaterial: 'Leather',
+      },
+      {
+        Fastening: 'Lace-Up',
+      },
+      {
+        rubber: 'Sole: rubber',
+      },
+      {
+        ProductCode: '39115101',
+      },
+      {
+        brand: 'Timberland',
+      },
+    ],
+    colours: [
+      {
+        colourName: 'Tan',
+        colourImage:
+          'https://raw.githubusercontent.com/sandramolina/assets/main/colors/color-red.png',
+      },
+      {
+        colourName: 'Black',
+        colourImage:
+          'https://raw.githubusercontent.com/sandramolina/assets/main/colors/color-hot-pink.png',
+      },
+    ],
+    isFavourite: false,
+  },
+];
