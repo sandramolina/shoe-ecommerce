534eb6541208a68a4faa8b4674276b6bea0a0e37
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-08-06 19:24:03 +0100

    adds color btns

--- src/features/Product/DetailedProduct/DetailedProduct.jsx
+++ src/features/Product/DetailedProduct/DetailedProduct.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Carousel, Col, Container, Row } from 'react-bootstrap';
+import { Button, Carousel, Col, Container, Row, Form } from 'react-bootstrap';
 
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
@@ -11,15 +11,25 @@ import './DetailedProduct.css';
 const DetailedProduct = () => {
   const { id } = useParams();
 
+  //Selection of product
   const product = useSelector((state) =>
     selectByProductId(state.productsState, Number(id))
   );
 
+  //Image carousel generation
   const imagesUrlList = product.images;
-  const carouselNodes = imagesUrlList.map((imgUrl) => (
-    <Carousel.Item>
+  const carouselNodes = imagesUrlList.map((imgUrl, i) => (
+    <Carousel.Item key={i}>
       <img className='d-block w-100' src={imgUrl} alt='Product' />
     </Carousel.Item>
+  ));
+
+  //Colours list
+  const productStocks = product.productStocks;
+  const colourList = productStocks.map((stockItem, i) => (
+    <Button variant='success' key={i} className='colour-btn-dp'>
+      {stockItem.colour.colourName}
+    </Button>
   ));
 
   return (
@@ -32,9 +42,21 @@ const DetailedProduct = () => {
           {product.brand}
           <h2>{product.title}</h2>
           <p>
-            Rating: {product.rating.averageRate}{' '}
+            Rating: {product.rating.averageRate}
             <span className='rate-count'>({product.rating.count} Reviews)</span>
           </p>
+          <Form>
+            <Form.Group className='mb-3' controlId='formColour'>
+              <Form.Label>Colors</Form.Label>
+              {colourList}
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='formSize'>
+              <Form.Label>Sizes</Form.Label>
+            </Form.Group>
+            <Button variant='success' type='submit'>
+              Add to Cart
+            </Button>
+          </Form>
         </Col>
       </Row>
       <Row id='bottom-section'>
