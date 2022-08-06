fa5e174c7cb88fa053ca8adeba9d794dfcad9292
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-08-06 12:36:36 +0100

    sarts dpr grid

--- src/features/Footer/Footer.jsx
+++ src/features/Footer/Footer.jsx
@@ -7,36 +7,34 @@ import './Footer.css';
 
 const Footer = () => {
   return (
-    <>
-      <Container className='footer' fluid>
-        <Row>
-          <Col className='text-center container-top'>
-            <Button className='top' />
-          </Col>
-        </Row>
-        <Row className='text-center'>
-          <Col>
-            <h2 className='social-media'>FOLLOW Golden Shoe</h2>
-            <Facebook size={35} className='social-media' />
-            <Twitch size={35} className='social-media' />
-            <Twitter size={35} className='social-media' />
-            <Instagram size={35} />
-          </Col>
-          <hr />
-          <Col>
-            <h3>GET HELP</h3>
-            <h3>ABOUT GOLDE SHOE</h3>
-            <h3>FIND US</h3>
-          </Col>
-        </Row>
-        <Row>
-          <Col className='text-center py-3'>
-            Made with 💜 by{' '}
-            <a href='https://github.com/sandramolina'>San Molina Palencia</a>
-          </Col>
-        </Row>
-      </Container>
-    </>
+    <Container className='footer' fluid>
+      <Row>
+        <Col className='text-center container-top'>
+          <Button className='top' />
+        </Col>
+      </Row>
+      <Row className='text-center'>
+        <Col>
+          <h2 className='social-media'>FOLLOW Golden Shoe</h2>
+          <Facebook size={35} className='social-media' />
+          <Twitch size={35} className='social-media' />
+          <Twitter size={35} className='social-media' />
+          <Instagram size={35} />
+        </Col>
+        <hr />
+        <Col>
+          <h3>GET HELP</h3>
+          <h3>ABOUT GOLDE SHOE</h3>
+          <h3>FIND US</h3>
+        </Col>
+      </Row>
+      <Row>
+        <Col className='text-center py-3'>
+          Made with 💜 by{' '}
+          <a href='https://github.com/sandramolina'>San Molina Palencia</a>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
--- src/features/Product/DetailedProduct/DetailedProduct.jsx
+++ src/features/Product/DetailedProduct/DetailedProduct.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 import { selectByProductId } from '../ProductSlice';
+
+import './DetailedProduct.css';
 
 const DetailedProduct = () => {
   const { id } = useParams();
@@ -12,7 +15,24 @@ const DetailedProduct = () => {
     selectByProductId(state.productsState, Number(id))
   );
 
-  return <div>{product.title}</div>;
+  return (
+    <Container id='detailed-product-container'>
+      <Row id='top-section'>
+        <Col>Carousel - images</Col>
+        <Col>
+          {product.brand}
+          <h2>{product.title}</h2>
+          <p>
+            Rating: {product.rating.averageRate}{' '}
+            <span className='rate-count'>({product.rating.count} Reviews)</span>
+          </p>
+        </Col>
+      </Row>
+      <Row id='bottom-section'>
+        <Col>{product.shortDescription}</Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default DetailedProduct;
