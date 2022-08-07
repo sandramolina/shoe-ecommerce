3301703f3665f92ef8bab42f34c5e350dcf7fa74
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-08-07 10:28:25 +0100

    creates cart modal slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productsState from '../features/Product/ProductSlice';
+import cartIsOpen from '../features/Cart/CartModal/CartModalSlice';
 
 export const store = configureStore({
-  reducer: { productsState },
+  reducer: { productsState, cartIsOpen },
 });
 
 export default store;
