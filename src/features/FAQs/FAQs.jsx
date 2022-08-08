5a2a40976f9d243acba3fda8afa2403b40ca365a
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-08-08 08:56:46 +0100

    completes delivery

--- src/features/FAQs/FAQs.jsx
+++ src/features/FAQs/FAQs.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import Delivery from './Delivery';
+
 const FAQs = () => {
-  return <div>FAQs</div>;
+  return (
+    <Tabs
+      defaultActiveKey='profile'
+      id='uncontrolled-tab-example'
+      className='mb-3'
+    >
+      <Tab eventKey='home' title='Delivery'>
+        <Delivery />
+      </Tab>
+      <Tab eventKey='returns' title='Returns'></Tab>
+      <Tab eventKey='contact' title='Contact Us'></Tab>
+      <Tab eventKey='orders' title='Orders' disabled></Tab>
+    </Tabs>
+  );
 };
 
 export default FAQs;
