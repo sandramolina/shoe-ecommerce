c381bdcdb9df258d0c661374a76ed75e31001d3a
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-08-09 15:28:18 +0100

    mc

--- src/features/Product/ProductGrid.jsx
+++ src/features/Product/ProductGrid.jsx
@@ -14,7 +14,7 @@ const ProductGrid = () => {
 
   return (
     <div className='product-grid'>
-      <h1 className='product-title'> Products</h1>
+      <h1 className='product-title'> All Products</h1>
       {productNodes.length !== 0 ? (
         <div className='products'>{productNodes} </div>
       ) : (
